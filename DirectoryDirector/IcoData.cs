5e601007ea95a9e65d738c8cb07346f70c7a9c84
David Sandström <xofasonic@outlook.com>
2024-04-14 13:35:20 +0200

    Custom ico file support. Handles duplicates and name collisions gracefully

--- DirectoryDirector/IcoData.cs
+++ DirectoryDirector/IcoData.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -9,7 +11,8 @@ namespace DirectoryDirector;
 
 public class IcoData
 {
-    public List<string[]> IcoDataList { get; }
+    public ObservableCollection<string[]> IcoDataList { get; }
+    public ObservableCollection<string[]> FavoriteList { get; }
 
     public IcoData()
     {
@@ -21,6 +24,33 @@ public class IcoData
         string[] icoPaths = Directory.GetFiles(basePath, "*.ico");
 
         // Create a list of icon data, each containing the path and name
-        IcoDataList = icoPaths.Select(icoPath => new[] { icoPath, Path.GetFileName(icoPath) }).ToList();
+        IcoDataList = new ObservableCollection<string[]>();
+        foreach (string icoPath in icoPaths)
+        { IcoDataList.Add(new[] { icoPath, Path.GetFileName(icoPath) }); }
+        
+        // Add Assets/Add.svg to start of list
+        FavoriteList = new ObservableCollection<string[]>();
+        string assetsPath = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) ?? throw new InvalidOperationException(), "Assets");
+        FavoriteList.Insert(0, new[] { Path.Combine(assetsPath, "Add.svg"), "Add…" });
+    }
+    
+    public void AddCustomIco(string icoPath)
+    {
+        // Add custom icon to the list
+        IcoDataList.Add(new[] { icoPath, Path.GetFileName(icoPath) });
+        PrintIcoData();
+    }
+    
+    public void AddFavorite(string[] icoData)
+    {
+        FavoriteList.Add(icoData);
+    }
+    
+    public void PrintIcoData()
+    {
+        foreach (string[] icoData in IcoDataList)
+        {
+            Debug.WriteLine(icoData[0] + " " + icoData[1]);
+        }
     }
 }
--- DirectoryDirector/MainWindow.xaml.cs
+++ DirectoryDirector/MainWindow.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using Windows.Storage;
 using Windows.UI;
 using Windows.UI.Popups;
 using Microsoft.UI;
@@ -11,18 +13,20 @@ using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Vanara.PInvoke;
+using WinRT.Interop;
+using FileAttributes = System.IO.FileAttributes;
 using Path = System.IO.Path;    
 
 namespace DirectoryDirector
 {
     public partial class MainWindow : Window
     {
-        private string[] folderList; // List of folders to apply the icon to
+        private string[] _folderList; // List of folders to apply the icon to
         
         // Initiated from context menu
         public MainWindow(string[] folderList)
         {
-            this.folderList = folderList;
+            this._folderList = folderList;
             Debug.WriteLine("arguments received");
             Debug.WriteLine(folderList);
             InitializeComponent();
@@ -38,7 +42,7 @@ namespace DirectoryDirector
         private void CopyIcoFile(string icoPath)
         {
             // Repeat for all selected folders
-            foreach (string folderPath in folderList)
+            foreach (string folderPath in _folderList)
             {
                 string pathOfCopy = Path.Combine(folderPath, Path.GetFileName(icoPath));
                 
@@ -90,6 +94,64 @@ namespace DirectoryDirector
             Shell32.SHGetSetFolderCustomSettings(ref pfcs, path, Shell32.FCS.FCS_FORCEWRITE);
             Shell32.SHChangeNotify(Shell32.SHCNE.SHCNE_UPDATEDIR, Shell32.SHCNF.SHCNF_PATHW, path);
         }
+        
+        private async void OpenFilePicker()
+        {
+            // Create a new file picker
+            var picker = new Windows.Storage.Pickers.FileOpenPicker
+            {
+                ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
+            };
+            picker.FileTypeFilter.Add(".ico");
+            
+            var hwnd = WindowNative.GetWindowHandle(this);     
+            InitializeWithWindow.Initialize(picker, hwnd);
+            var file = await picker.PickSingleFileAsync();
+
+            // No file picked
+            if (file == null) return;
+            
+            // Check if exact same file exists within CachedIcons already
+            StorageFolder folder = await StorageFolder.GetFolderFromPathAsync(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) 
+                ?? throw new InvalidOperationException(), "CachedIcons"));
+            foreach (var fileInFolder in await folder.GetFilesAsync())
+            {
+                if (!(await File.ReadAllBytesAsync(file.Path)).SequenceEqual(
+                        await File.ReadAllBytesAsync(fileInFolder.Path))) continue;
+                
+                // Same, use existing file
+                CopyIcoFile(fileInFolder.Path);
+                return;
+            }
+            
+            // TODO: Clean up, refactor
+            // TODO: Next: Favorites, delete with context menu? Sorting of standard list 
+            
+            // Different, copy over new file
+            await file.CopyAsync(folder, file.Name, NameCollisionOption.GenerateUniqueName);
+            if (MainGrid.DataContext is IcoData icoData)
+            {
+                icoData.AddCustomIco(folder.Path + "\\" + file.Name);
+                CopyIcoFile(folder.Path + "\\" + file.Name);
+            }
+        }
+
+        private void GridClickHandler(string clickedTile)
+        {
+            // If the clicked tile is "Add…", open the file picker
+            if (clickedTile == "Add…")
+            {
+                OpenFilePicker();
+            }
+            else
+            {
+                // Append the file name to the path of CachedIcons within the exe directory
+                string path = clickedTile;
+                path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) 
+                                    ?? throw new InvalidOperationException(), "CachedIcons", path);
+                CopyIcoFile(path);
+            }
+        }
 
         // Handle the click event for the custom folder buttons
         private void FolderButton_OnMouseLeftButtonDown(object sender, RoutedEventArgs e)
@@ -97,15 +159,12 @@ namespace DirectoryDirector
             // Get the clicked item, a StackPanel containing a TextBlock and an Image
             StackPanel stackPanel = (StackPanel)sender;
             TextBlock textBlock = (TextBlock)stackPanel.Children[1];
-            string path = textBlock.Text;
+            string clickedTile = textBlock.Text;
             
-            // Append the file name to the path of CachedIcons within the exe directory
-            path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) 
-                                ?? throw new InvalidOperationException(), "CachedIcons", path);
-            
-            CopyIcoFile(path);
+            GridClickHandler(clickedTile);
         }
         
+        // Hover enter animation
         private void StackPanel_PointerEntered(object sender, PointerRoutedEventArgs e)
         {
             if (sender is StackPanel stackPanel)
@@ -114,6 +173,7 @@ namespace DirectoryDirector
             }
         }
 
+        // Hover exit animation
         private void StackPanel_PointerExited(object sender, PointerRoutedEventArgs e)
         {
             if (sender is StackPanel stackPanel)
